49c081109438c960f07bac9ef7a081ae8a5012ef
Khoaz12345 <hophannguyenkhoa@gmail.com>
2024-12-17 07:34:24 +0700

    Add files via upload

---
+++ LoopWhile/src/basic/dev/gttv.java
@@ -0,0 +1,15 @@
+package basic.dev;
+import java.util.Scanner;
+public class gttv {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner Sc=new Scanner(System.in);
+System.out.print("nhap vao so nguyen n");
+int n=Sc.nextInt();
+System.out.print("so vua nhap la "+n);	
+Sc.close();
+	
+	}
+
+}
